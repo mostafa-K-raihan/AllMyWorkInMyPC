27822d38e105423dae4f31dfdc5ad702278d19e1
raihan <mraihan29@gmail.com>
2025-02-21 22:50:37 +0700

    Add a terminal game

---
+++ code/game/test/README.md
@@ -0,0 +1,19 @@
+# What this is about?
+
+This is a simple termial game. Following a youtube tutorial, channel name - Tom on the internet
+
+## Dependencies
+
+None.
+Well you need node js installed tbh, and a package manager.
+
+### Milestones
+
+- Make an executable
+
+
+### Run the game
+
+```
+chmod +x index.js
+```
---
+++ code/game/test/index.js
@@ -0,0 +1,76 @@
+#!/usr/bin/env node
+
+import process from 'node:process'
+import { emitKeypressEvents } from 'readline'
+
+
+function setupTerminal() {
+  
+  emitKeypressEvents(process.stdin)
+  process.stdin.setRawMode(true)
+  
+  // go to the alrernate screen
+  process.stdout.write('\x1b[?1049h')
+  process.stdout.write('\x1b[2J');     // Clear the alternate screen
+  process.stdout.write('\x1b[?25l'); // Hide the cursor
+}
+
+
+function gameover() {
+  process.stdout.write('\x1b[?1049l'); // Restore normal screen
+  process.stdout.write('\x1b[?25h'); // Restore cursor
+  process.exit()
+	return
+}
+
+const state = { 
+	x: 0, 
+	y: 0, 
+	targetX: 9, 
+	targetY: 19, 
+	interval: setInterval(function () {
+	  moveTarget();	  
+        }, 100) 
+}
+
+setupTerminal()
+
+process.stdin.on('keypress', function (_, key) {
+  if (key.name === 'q') { gameover() }
+  if (key.name === 'left') { state.x -- }
+  if (key.name === 'right') { state.x ++ }
+  if (key.name === 'up') { state.y -- }
+  if (key.name === 'down') { state.y ++ }
+
+  update()
+})
+
+
+function moveTarget() {
+	const x = Math.random()
+	if (x > .5) { state.targetX ++ }
+	else { state.targetX -- }
+
+	const y = Math.random()
+	if (y > .5) { state.targetY ++ }
+	else state.targetY --
+
+
+	update()
+
+}
+
+function update() {
+	if (state.x === state.targetX && state.y === state.targetY) { gameover() }
+	render()
+}
+function render() {
+  console.clear()
+  process.stdout.cursorTo(state.x, state.y)
+  process.stdout.write('X')
+  process.stdout.cursorTo(state.targetX, state.targetY)
+  process.stdout.write('0')
+}
+
+
+
